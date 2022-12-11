cfd3148eb5aed018b1f5cc50a6115899ddbee086
Bhanu rai <bhanurai001@gmail.com>
2022-12-11 08:28:15 +0545

    widgets

--- lib/dashboard/widgets/commons.dart
+++ lib/dashboard/widgets/commons.dart
@@ -1,7 +1,7 @@
-//  import 'package:flutter/material.dart';
-//
-//
-// const red = Colors.red;
-// const white = Colors.white;
-// const black = Colors.black;
-// const grey = Colors.grey;
+ import 'package:flutter/material.dart';
+
+
+const red = Colors.red;
+const white = Colors.white;
+const black = Colors.black;
+const grey = Colors.grey;
--- lib/dashboard/widgets/title.dart
+++ lib/dashboard/widgets/title.dart
@@ -1,17 +1,17 @@
-// import 'package:flutter/material.dart';
-//
-// import '../widgets/commons.dart';
-//
-// class CustomText extends StatelessWidget {
-//   final String text;
-//   final double size;
-//   // final Color colors;
-//   // final FontWeight weight;
-//
-//
-//   CustomText({required this.text, required this.size,});//required this.colors,required this.weight,
-//   @override
-//   Widget build(BuildContext context) {
-//     return Text(text,style: TextStyle(fontSize: size ?? 16,),);//color: colors ?? black, fontWeight: FontWeight.normal
-//   }
-// }
+import 'package:flutter/material.dart';
+
+import '../widgets/commons.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double size;
+  // final Color colors;
+  // final FontWeight weight;
+
+
+  CustomText({required this.text, required this.size,});//required this.colors,required this.weight,
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,style: TextStyle(fontSize: size ?? 16,),);//color: colors ?? black, fontWeight: FontWeight.normal
+  }
+}
