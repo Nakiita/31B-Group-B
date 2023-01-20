96e535a4935a769968bcdbb61d236b7a00062447
Aashisss <aashishshah@Aashishs-MacBook-Air.local>
2023-01-20 11:53:36 +0545

    file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,73 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
-import 'package:hunger_cravings/Login/login.dart';
+import 'package:flutter_dark_mode_animation/theme_service.dart';
 
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(MyApp());
-// }
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-  );
-  runApp(MyApp());
+  final themeService = await ThemeService.instance;
+  var initTheme = themeService.initial;
+  runApp(MyApp(theme: initTheme));
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  MyApp({required this.theme});
+
+  final ThemeData theme;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      // home:  const LoginScreens(),
+    return ThemeProvider(
+      initTheme: theme,
+      builder: (_, theme) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: theme,
+          home: HomePage(),
+        );
+      },
     );
   }
 }
+
+
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hunger_cravings/Login/login.dart';
+//
+// // Future<void> main() async {
+// //   WidgetsFlutterBinding.ensureInitialized();
+// //   await Firebase.initializeApp();
+// //   runApp(MyApp());
+// // }
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//   );
+//   runApp(MyApp());
+// }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // Try running your application with "flutter run". You'll see the
+//         // application has a blue toolbar. Then, without quitting the app, try
+//         // changing the primarySwatch below to Colors.green and then invoke
+//         // "hot reload" (press "r" in the console where you ran "flutter run",
+//         // or simply save your changes to "hot reload" in a Flutter IDE).
+//         // Notice that the counter didn't reset back to zero; the application
+//         // is not restarted.
+//         primarySwatch: Colors.blue,
+//       ),
+//       // home:  const LoginScreens(),
+//     );
+//   }
+// }
