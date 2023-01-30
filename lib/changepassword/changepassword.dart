26ec3ff4af70d09d90e250d1752b1f3351a41a87
Aashisss <aashishshah@Aashishs-MacBook-Air.local>
2023-01-30 11:26:17 +0545

    sizedbox with container added

--- lib/changepassword/changepassword.dart
+++ lib/changepassword/changepassword.dart
@@ -145,6 +145,49 @@ class _ChangePasswordState extends State<ChangePassword> {
                             ),
                           ),
                         ),
+                        SizedBox(height: constraints.maxHeight * 0.04),
+                        Container(
+                          height: constraints.maxHeight * 0.12,
+                          decoration: BoxDecoration(
+                              color: const Color(0xffB4B4B4).withOpacity(0.6),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Center(
+                              child: TextFormField(
+                                controller: _currentPasswordController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return "Please enter password";
+                                  }
+                                },
+                                obscureText: _isVisible ? false : true,
+                                decoration: InputDecoration(
+                                    prefixIcon: Icon(Icons.lock),
+                                    filled: true,
+                                    fillColor: Colors.grey.shade50,
+                                    hintText: ' Confirm Password ',
+                                    suffixIcon: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _isVisible = !_isVisible;
+                                        });
+                                      },
+                                      icon: Icon(
+                                        _isVisible
+                                            ? Icons.visibility
+                                            : Icons.visibility_off,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(40),
+                                        borderSide:
+                                        BorderSide(color: Colors.black12))),
+                              ),
+                            ),
+                          ),
+                        ),
 
                       ],
                     );
