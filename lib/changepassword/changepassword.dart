f82cdffc52dc76081b7412fca111416d168ea354
Aashisss <aashishshah@Aashishs-MacBook-Air.local>
2023-01-30 11:17:10 +0545

    :hammer: forgot password

---
+++ lib/changepassword/changepassword.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChangePassword extends StatefulWidget {
+  const ChangePassword({Key? key}) : super(key: key);
+
+  @override
+  State<ChangePassword> createState() => _ChangePasswordState();
+}
+
+class _ChangePasswordState extends State<ChangePassword> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text(
+          "Change password",
+        ),
+      ),
+    );
+  }
+}
