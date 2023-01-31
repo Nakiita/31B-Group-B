6dce5621f15df1a8e69622b297be2d1356ebc495
Nakiita <nakitarayamajhi27@gmail.com>
2023-01-31 20:28:38 +0545

    Color changed

--- lib/DetailsScreen/const/AppColors.dart
+++ lib/DetailsScreen/const/AppColors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
-class AppColors{
-  static const Color deep_orange = Color(0xFFFF6B6B);
+class AppColors {
+  static const Color Black = Color.fromARGB(255, 36, 34, 34);
 }
